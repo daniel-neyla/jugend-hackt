5ca366d76b54f6e9da55cc6f4444b1009fdfec15
thomas <thornung@gmail.com>
2019-09-27 12:41:10 +0200

    liste hinzugefuegt

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-
+list1 = [1,2,3,4,5]
 dict1 = {'a': 'bla blub', 'b': 'hallo'}
 print("hello world")
 
 for k, v in dict1.items():
     print('{}: {}'.format(k, v))
+
+for l in list1:
+    print('square: {}'.format(l**2))
